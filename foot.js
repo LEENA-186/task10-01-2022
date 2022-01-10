720b8fdceb4e6be79c235f6d0d154d7976efd8c9
Leena Madhuri <74544568+LEENA-186@users.noreply.github.com>
2022-01-10 19:52:17 +0530

    Add files via upload

---
+++ foot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render(){
+        return(
+            <div>
+                <h1>FOOTER</h1>
+                <p>here we include the content we want to add in our footer </p>
+            </div>
+        );
+    }
+}
+export default Footer;
---
+++ head.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () =>{
+    return(
+
+        <div>
+            <ul>
+                <li>HOME</li>
+                <p>hello welcome to home page</p>
+                
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
